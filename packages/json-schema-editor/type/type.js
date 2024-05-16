a4482407e413db2f4375fb317f575fd4e2674321
lzodo <869664233@qq.com>
2024-05-16 10:05:01 +0800

    save

--- packages/json-schema-editor/type/type.js
+++ packages/json-schema-editor/type/type.js
@@ -4,14 +4,14 @@ import _array from './array'
 import _boolean from './boolean'
 import _integer from './integer'
 import _number from './number'
-const TYPE_NAME = ['string', 'number', 'integer','object', 'array',  'boolean']
+const TYPE_NAME = ['String', 'Number', 'Integer', 'Object', 'Array', 'Boolean']
 
 const TYPE = {
-    'object': _object,
-    'string': _string,
-    'array': _array,
-    'boolean': _boolean,
-    'integer': _integer,
-    'number': _number
+  Object: _object,
+  String: _string,
+  Array: _array,
+  Boolean: _boolean,
+  Integer: _integer,
+  Number: _number,
 }
-export {TYPE ,TYPE_NAME}
+export { TYPE, TYPE_NAME }
