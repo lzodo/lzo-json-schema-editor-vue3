7d659228d64aa6ec1a13cd4dd54c9251fe4ee578
lzodo <869664233@qq.com>
2024-06-14 08:44:38 +0800

    ant3版本

--- examples/main.js
+++ examples/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import JsonSchemaEditor from '../packages/index'
-import { Modal} from 'ant-design-vue'
+import { Modal } from 'ant-design-vue'
+
+// import Antd from 'ant-design-vue'
+// import 'ant-design-vue/dist/antd.css'
+
 createApp(App).use(JsonSchemaEditor).use(Modal).mount('#app')
